cb72f6cb42ef048e93223a158e53d41d7e9fab04
daivya17 <daivya.cs23@bmsce.ac.in>
2024-12-14 16:53:41 +0530

    Create quadratic.java

---
+++ LAB1/quadratic.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class quad{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the coefficients:");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		int d=b*b-4*a*c;
+		if(d==0){
+			System.out.println("Roots are equal");
+			System.out.println("Roots are:");
+			System.out.println(-b/2*a);
+}
+		else if(d>0){
+			System.out.println("Roots are unique");
+			System.out.println((-b+Math.sqrt(d))/(2*a));
+			System.out.println((-b-Math.sqrt(d))/(2*a));
+
+}	
+		else{
+			System.out.println("No real roots");
+}
+	}
+}
