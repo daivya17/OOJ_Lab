c3127b47186fb7a9f736053de0e960b796619da8
daivya17 <daivya.cs23@bmsce.ac.in>
2024-12-14 17:20:11 +0530

    Create Mainmarks.java

---
+++ LAB6/Mainmarks.java
@@ -0,0 +1,29 @@
+import CIE.*;
+import SEE.*;
+import java.util.Scanner;
+public class Mainmarks {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter no of students:");
+        int n = s.nextInt();
+        s.nextLine();
+        for(int i=0;i<n;i++){
+            System.out.println("name:");
+            String name = s.nextLine();
+            System.out.println("usn:");
+            String usn = s.nextLine();
+            System.out.println("Sem:");
+            int sem = s.nextInt();
+            s.nextLine();
+            Externals e = new Externals(usn,name,sem);
+            e.displaydetails();
+            e.getintmarks();
+            e.dispintmarks();
+            e.getextmarks();
+            e.displayextmarks();
+            e.totalmarks();
+            System.out.println("Daivya Priyankkumar Shah");
+            System.out.println("1BM23CS084");
+        }
+    }
+}
